552405b042bcb25f604227a8a969f9a98a0cf408
Adrán Negrea <barackhu1@gmail.com>
2025-08-30 16:59:05 +0200

    upload: adding main.py, auth.py and views.py with blueprints

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import auth
+import views
+
+app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(views.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
