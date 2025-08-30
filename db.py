8a4f87ffe80df67a7594a1724cfff6ac5660278d
Adrán Negrea <barackhu1@gmail.com>
2025-08-30 16:57:50 +0200

    fix: added config.py for loading .env variables

--- db.py
+++ db.py
@@ -1,18 +1,21 @@
-import os
-
 from dotenv import load_dotenv
 from psycopg2 import connect
 from psycopg2._psycopg import connection
 from typing import Any, Generator
 
+from config import (DB_NAME,
+                    DB_USER,
+                    DB_PASSWORD,
+                    DB_HOST)
+
 load_dotenv()
 
 def get_db() -> Generator[connection, Any, None]:
     conn = connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("USER"),
-        password=os.getenv("PASSWORD"),
-        host=os.getenv("HOST"),
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
     )
     try:
         yield conn
