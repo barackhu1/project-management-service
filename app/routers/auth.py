ad929a973d1f3e15b132161e465c89c889ad2e8e
Adrán Negrea <barackhu1@gmail.com>
2025-09-08 12:15:43 +0200

    fix: status code added

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.schemas.schemas import UserCreate, UserLogin
 from app.crud.users import insert_user, get_user_by_username
@@ -13,12 +13,13 @@ def register_user(user: UserCreate, conn = Depends(get_db)):
     try:
         db_user = insert_user(conn, user.username, hashed)
         return {
+            "status_code": status.HTTP_201_CREATED,
             "message": "User registered successfully",
             "user": db_user,
         }
     except Exception as e:
         raise HTTPException(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Username already exists"
         )
 
@@ -28,23 +29,24 @@ def login_user(form: UserLogin, conn = Depends(get_db)):
     password = form.password
 
     if not username or not password:
-        raise HTTPException(status_code=400, detail="Username or password is required")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username or password is required")
 
     user = get_user_by_username(conn, username)
 
     if not user:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     user_id = user[0]
     stored_password_hash = user[2]
 
     if not verify_password(password, stored_password_hash):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     token_data = {"user_id": user_id, "username": username}
     access_token = create_access_token(token_data)
 
     return {
+        "status_code": status.HTTP_200_OK,
         "message": "Login successful",
         "user_id": user_id,
         "username": username,
